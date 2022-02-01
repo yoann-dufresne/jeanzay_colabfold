bd82f3697dfb9cdd9361bad1cc0ec9a2c1bcc6e9
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-02-01 16:21:13 +0100

    jean zay slurm scripts

--- scripts/submit_array_template.sh
+++ scripts/submit_array_template.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=job-array   # nom du job
-#SBATCH --ntasks=1             # Nombre total de processus MPI
-#SBATCH --ntasks-per-node=1    # Nombre de processus MPI par noeud
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=8
-# Dans le vocabulaire Slurm "multithread" fait référence à l'hyperthreading.
-#SBATCH --hint=nomultithread   # 1 processus MPI par coeur physique (pas d'hyperthreading)
-#SBATCH --time=00:30:00        # Temps d’exécution maximum demande (HH:MM:SS)
-#SBATCH --output=/linkhome/rech/genpro01/ubu39dm/yoann/test2/%a.out  # Nom du fichier de sortie contenant l'ID et l'indice
-#SBATCH --error=/linkhome/rech/genpro01/ubu39dm/yoann/test2/%a.err   # Nom du fichier d'erreur (ici commun avec la sortie)
-#SBATCH --array=0-49%30        # 50 travaux en tout mais 30 travaux max dans la file
+#SBATCH --job-name=Serratus-Colabfold
+#SBATCH --ntasks=1                     # total number of process
+#SBATCH --ntasks-per-node=1            # Number of process per node
+#SBATCH --gres=gpu:1                   # GPU
+#SBATCH --cpus-per-task=8              # CPU
+#SBATCH --hint=nomultithread           # No hyperthreading
+#SBATCH --time=00:30:00                # Max exec time (HH:MM:SS) TIME
+#SBATCH --output=/linkhome/rech/genpro01/ubu39dm/yoann/test2/%a.out  # STDOUT
+#SBATCH --error=/linkhome/rech/genpro01/ubu39dm/yoann/test2/%a.err   # STDERR
+#SBATCH --array=0-49%30                # 50 travaux en tout mais 30 travaux max dans la file
 
-# on se place dans le répertoire de soumission
-cd /linkhome/rech/genpro01/ubu39dm/yoann
+module load python/3.8.8 cuda/11.2
+export PATH=$PATH:/gpfs7kw/linkhome/rech/genpro01/ubu39dm/.local/bin/
 
-# Execution du binaire "mon_exe" avec des donnees differentes pour chaque travail
-# La valeur de ${SLURM_ARRAY_TASK_ID} est differente pour chaque travail.
-srun sh /linkhome/rech/genpro01/ubu39dm/yoann/scripts/folder_run.sh
+cd /linkhome/rech/genpro01/ubu39dm/yoann # WORKDIR
+
+srun sh /linkhome/rech/genpro01/ubu39dm/yoann/scripts/job.sh # JOB
