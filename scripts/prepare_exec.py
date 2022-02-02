54fff9782a893d754e7292ab7d7d8509d1320be0
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-02-02 15:32:49 +0100

    submit script generation 1st test

--- scripts/prepare_exec.py
+++ scripts/prepare_exec.py
@@ -1,4 +1,4 @@
-from os import path, listdir
+from os import path, listdir, mkdir
 from shutil import rmtree
 
 import sys
@@ -10,7 +10,7 @@ import shutil
 
 
 
-def generate_submit(submit_dir, subdir_prefix, status_file):
+def generate_submit(submit_dir, subdir_prefix, status_file, submit_template, args):
     # Detect job candidates
     job_idxs = [int(f[len(subdir_prefix):]) for f in listdir(submit_dir) if f.startswith(subdir_prefix)]
     job_idxs = set(job_idxs)
@@ -28,15 +28,50 @@ def generate_submit(submit_dir, subdir_prefix, status_file):
             if num_mol == num_done:
                 job_idx = int(run[len(subdir_prefix):])
                 job_idxs.remove(job_idx)
+            # TODO: Remove molecule done from an unfinished job
 
-    # Group jobs for submition
+    if len(job_idxs) == 0:
+        print("No job to be done", file=sys.stderr)
+        exit(0)
+
     job_idxs = list(job_idxs)
     job_idxs.sort()
-
-    print(job_idxs)
+    # Group jobs for submition
+    start_job_idx = job_idxs[0]
+    end_job_idx = start_job_idx
+    grouped_job = []
+    for i, job_idx in enumerate(job_idxs[:-1]):
+        # if directly the next
+        if job_idxs[i+1] == job_idx +1:
+            end_job_idx = job_idxs[i+1]
+        # Create new segment
+        else:
+            grouped_job.append((start_job_idx, end_job_idx))
+            start_job_idx = end_job_idx = job_idxs[i+1]
+    grouped_job.append((start_job_idx, end_job_idx))
+    grouped_job = [str(x[0]) if x[0] == x[1] else f"{x[0]}-{x[1]}" for x in grouped_job]
 
     # Write the submition script
-
+    logdir = path.join(submit_dir, "logs")
+    if not path.exists(logdir): mkdir(logdir)
+    slurmfile = path.join(submit_dir, "submit.slurm")
+    with open(submit_template) as template, open(slurmfile, "w") as slurm:
+        for line in template:
+            key = line.strip().split("# ")[-1]
+            if key == "RAM": print(f"#SBATCH --mem={args.ram}", file=slurm)
+            elif key == "CPU": print(f"#SBATCH --cpus-per-task={args.num_cores}", file=slurm)
+            elif key == "TIME":
+                print(f"#SBATCH --time={min(100, args.max_mol_per_job)}:00:00", file=slurm)
+            elif key == "STDOUT":
+                print(f"#SBATCH --output={path.abspath(path.join(logdir, '%a.out'))}", file=slurm)
+            elif key == "STDERR":
+                print(f"#SBATCH --output={path.abspath(path.join(logdir, '%a.err'))}", file=slurm)
+            elif key == "ARRAY":
+                print(f"#SBATCH --array={','.join(grouped_job)}%{args.num_gpu}", file=slurm)
+            elif key == "WORKDIR": print(f"cd {path.abspath(submit_dir)}", file=slurm)
+            elif key == "JOB": print(f"srun sh ./job.sh", file=slurm)
+            else:
+                print(line.strip(), file=slurm)
 
 
 if __name__ == "__main__":
@@ -48,7 +83,6 @@ if __name__ == "__main__":
     parser.add_argument('--ram', type=int, default=16, help='Number of RAM Go for each job')
     parser.add_argument('--num-cores', type=int, default=4, help='Number of cores to use per job')
     parser.add_argument('--max-mol-per-job', '-m', type=int, default=10, help='Maximum number of molecule to fold using the same job')
-    parser.add_argument('--outdir', '-o', type=str, default="./", help='Directory where the scripts will be generated.')
 
     args = parser.parse_args()
 
@@ -68,7 +102,8 @@ if __name__ == "__main__":
     sp.run(command.split(" "), stdin=sys.stdin, stderr=sys.stderr)    
 
     # Step 3 - Generate slum files
-    generate_submit(args.input_dir, args.subdir_prefix, path.join(tmp_stats, "status.tsv"))
+    slurm_template = path.join(script_directory, "submit_array_template.sh")
+    generate_submit(args.input_dir, args.subdir_prefix, path.join(tmp_stats, "status.tsv"), slurm_template, args)
     shutil.copyfile(path.join(script_directory, "job.sh"), path.join(args.input_dir, "job.sh"))
 
     # Step 4 - Clean dir
--- scripts/submit_array_template.sh
+++ scripts/submit_array_template.sh
@@ -3,12 +3,13 @@
 #SBATCH --ntasks=1                     # total number of process
 #SBATCH --ntasks-per-node=1            # Number of process per node
 #SBATCH --gres=gpu:1                   # GPU
-#SBATCH --cpus-per-task=8              # CPU
 #SBATCH --hint=nomultithread           # No hyperthreading
-#SBATCH --time=00:30:00                # Max exec time (HH:MM:SS) TIME
+#SBATCH --mem=2                        # RAM
+#SBATCH --cpus-per-task=1              # CPU
+#SBATCH --time=00:30:00                # Max exec time (HH:MM:SS) # TIME
 #SBATCH --output=/linkhome/rech/genpro01/ubu39dm/yoann/test2/%a.out  # STDOUT
 #SBATCH --error=/linkhome/rech/genpro01/ubu39dm/yoann/test2/%a.err   # STDERR
-#SBATCH --array=0-49%30                # 50 travaux en tout mais 30 travaux max dans la file
+#SBATCH --array=0-49%30                # 50 travaux en tout mais 30 travaux max dans la file # ARRAY
 
 module load python/3.8.8 cuda/11.2
 export PATH=$PATH:/gpfs7kw/linkhome/rech/genpro01/ubu39dm/.local/bin/
