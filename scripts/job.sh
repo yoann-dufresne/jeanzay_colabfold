ad251bb7295e7ef06c97d8cfa7c49ee366b612f5
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-02-04 18:37:55 +0100

    delete unnecessary files + compress output

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 mkdir result_${SLURM_ARRAY_TASK_ID}
+echo "colabfold_batch split_${SLURM_ARRAY_TASK_ID} result_${SLURM_ARRAY_TASK_ID}"
+# Run colabfold
 colabfold_batch split_${SLURM_ARRAY_TASK_ID} result_${SLURM_ARRAY_TASK_ID}
+# Delete unneeded files
+cd "result_${SLURM_ARRAY_TASK_ID}"
+rm cite.bibtex config.json *.png *_rank_[2-5]_*
+# TODO: TM-Align
+cd ..
+tar -czf result_${SLURM_ARRAY_TASK_ID}.tar.gz result_${SLURM_ARRAY_TASK_ID}/ --remove-files
+rm -r split_${SLURM_ARRAY_TASK_ID}
