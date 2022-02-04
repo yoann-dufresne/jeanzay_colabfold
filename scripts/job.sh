08deabed0404ac07a541c4d5cef4ac66d5174ccb
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-02-04 23:51:54 +0100

    add TM-Align

--- scripts/job.sh
+++ scripts/job.sh
@@ -7,7 +7,8 @@ colabfold_batch split_${SLURM_ARRAY_TASK_ID} result_${SLURM_ARRAY_TASK_ID}
 # Delete unneeded files
 cd "result_${SLURM_ARRAY_TASK_ID}"
 rm cite.bibtex config.json *.png *_rank_[2-5]_*
-# TODO: TM-Align
+PATH=$PATH:~/.local/bin
+python3 /gpfswork/rech/rep/ubu39dm/softwares/palmfold/palmfold.py -p /gpfswork/rech/rep/ubu39dm/softwares/palmfold/pol/ -t 0.9 -d .
 cd ..
 tar -czf result_${SLURM_ARRAY_TASK_ID}.tar.gz result_${SLURM_ARRAY_TASK_ID}/ --remove-files
 rm -r split_${SLURM_ARRAY_TASK_ID}
