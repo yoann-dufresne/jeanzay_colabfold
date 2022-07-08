0168d0b854f82020f4f0043fa55797083c28f228
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-07-08 15:14:30 +0200

    fold update

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -12,4 +12,4 @@ mkdir -p $2
 awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' $1 |\
 awk -F '\t' '{printf("%d\t%s\n",length($2),$0);}' |\
 sort -k1,1n | cut -f 2- | tr "\t" "\n" |
-./scripts/faSplit about /dev/stdin 1500000 $2
+./scripts/faSplit about /dev/stdin 100000 $2
