855ca1ca528adb72022c493d190281c781d9accd
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-07-18 11:24:39 +0200

    msa scripts update

--- pasteur_run_snakemake.sh
+++ pasteur_run_snakemake.sh
@@ -3,7 +3,7 @@
 module load Python/3.8.3 cuda/11.2
 
 snakemake --snakefile=Snakefile_align --unlock
-snakemake --snakefile=Snakefile_align --debug-dag --cluster "sbatch -c {threads} --qos={params.qos} -p {params.partition} --job-name={params.job_name} --mem={params.mem} --output=out/{params.job_name}_%j.out --error=out/{params.job_name}_%j.err" --jobs=50 --restart-times=3 --show-failed-logs
+snakemake --snakefile=Snakefile_align --cluster "sbatch -c {threads} --qos={params.qos} -p {params.partition} --job-name={params.job_name} --mem={params.mem} --output=out/{params.job_name}_%j.out --error=out/{params.job_name}_%j.err" --jobs=50 --restart-times=3 --show-failed-logs
 
 
 # for f in data/*.ready
