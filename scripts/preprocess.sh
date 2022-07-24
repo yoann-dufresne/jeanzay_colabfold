a62b4a770809abeaebb2225780c6057372a1e339
Yoann  DUFRESNE <yoann.dufresne0@gmail.com>
2022-07-24 16:34:09 +0200

    remove leading 0 on files

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -13,3 +13,11 @@ awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {
 awk -F '\t' '{printf("%d\t%s\n",length($2),$0);}' |\
 sort -k1,1n | cut -f 2- | tr "\t" "\n" |
 ./scripts/faSplit about /dev/stdin 100000 $2
+
+for var in "00" "01" "02" "03" "04" "05" "06" "07" "08" "09"
+do
+    if [ -f "${2%/}/$var.fa" ]; then
+        echo "mv ${2%/}/$var.fa ${2%/}/${var:1}.fa"
+        mv ${2%/}/$var.fa ${2%/}/${var:1}.fa
+    fi
+done
