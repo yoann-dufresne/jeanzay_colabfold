fdabcea364aa7679b6ea8339d3673ade11ed7baa
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-08-03 11:29:59 +0200

    rename realigned files

---
+++ scripts/rename_realigned.py
@@ -0,0 +1,20 @@
+from os import listdir, rename, path
+
+lib_path = path.join("data", "CFDL_split")
+
+for sample_dir in listdir(lib_path):
+    sample_path = path.join(lib_path, sample_dir)
+    fold_path = path.join(sample_path, "fold_split")
+
+    if not path.exist(fold_path):
+        continue
+
+    for split_dir in listdir(fold_path):
+        split_path = path.join(fold_path, split_dir)
+
+        for file in split_path:
+            if "realign.pdb" in file:
+                rename(
+                    path.join(split_path, file),
+                    path.join(split_path, file[:file.find(".pdb")+4])
+                )
