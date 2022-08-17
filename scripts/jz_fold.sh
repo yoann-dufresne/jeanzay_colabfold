a88e1bf25358e38c05fcf9e1b65bc11a2dc1262d
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-08-17 16:33:53 +0200

    rollback tmp dir

--- scripts/jz_fold.sh
+++ scripts/jz_fold.sh
@@ -2,9 +2,6 @@
 
 module load python/3.9.12 cuda/11.2
 mkdir -p /gpfsscratch/rech/yph/uep61bl/data/tmp/
-RAND_DIR="/gpfsscratch/rech/yph/uep61bl/data/tmp/$RANDOM$RANDOM$RANDOM$RANDOM"
-export TMPDIR=$RAND_DIR
+export TMPDIR=/gpfsscratch/rech/yph/uep61bl/data/tmp/
 
 python3 scripts/jz_fold.py $sample_path $SLURM_ARRAY_TASK_ID
-
-rm -rf $RAND_DIR
