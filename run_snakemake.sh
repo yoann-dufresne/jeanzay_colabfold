66855ff921cadfbe1f0b43f97c2130b99942227b
Yoann  DUFRESNE <yoann.dufresne0@gmail.com>
2022-08-22 11:28:09 +0200

    clean the folding process

--- run_snakemake.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-module load python/3.9.12 cuda/11.2
-
-snakemake --unlock --snakefile=Snakefile_fold
-snakemake --snakefile=Snakefile_fold --cluster "sbatch -c {threads} --cpus-per-task={params.cpus} --qos={params.qos} -p {params.partition} -A {params.account} {params.options} --time={params.time} --job-name={params.job_name} --output=out/{params.job_name}_%j.out --hint=nomultithread --error=out/{params.job_name}_%j.err" --jobs=2000 --max-jobs-per-second=10 --max-status-checks-per-second=10 --restart-times=1 --show-failed-logs --resources io=20 --resources gpu=1100
